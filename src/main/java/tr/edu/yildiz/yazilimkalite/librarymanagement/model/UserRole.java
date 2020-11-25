eb22b81e9135ba12479cb2497a7a07d2e07576ce
clfv5 <bedirhanakcay@icloud.com>
2020-11-26 00:55:46 +0300

    UserPermission entitiy deleted. 	UserRole edited. UserPermission list deleted

--- src/main/java/tr/edu/yildiz/yazilimkalite/librarymanagement/model/UserRole.java
+++ src/main/java/tr/edu/yildiz/yazilimkalite/librarymanagement/model/UserRole.java
@@ -1,16 +1,12 @@
 package tr.edu.yildiz.yazilimkalite.librarymanagement.model;
 
 import java.sql.Timestamp;
-import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -26,17 +22,6 @@ public class UserRole {
     @Column(nullable = false)
     @CreationTimestamp
     private Timestamp creationTime;
-
-    /*
-     * User who granted the role to the grantee
-     */
-    @OneToOne
-    @JoinColumn
-    private User grantor;
-
-    @OneToMany
-    @JoinColumn(name = "role_id")
-    private List<UserPermission> permissions;
 
     public UserRole() {
         super();
@@ -66,26 +51,9 @@ public class UserRole {
         this.creationTime = creationTime;
     }
 
-    public User getGrantor() {
-        return grantor;
-    }
-
-    public void setGrantor(User grantor) {
-        this.grantor = grantor;
-    }
-
-    public List<UserPermission> getPermissions() {
-        return permissions;
-    }
-
-    public void setPermissions(List<UserPermission> permissions) {
-        this.permissions = permissions;
-    }
-
     @Override
     public String toString() {
-        return "UserRole [creationTime=" + creationTime + ", grantor=" + grantor + ", id=" + id + ", permissions="
-                + permissions + ", role=" + role + "]";
+        return "UserRole [creationTime=" + creationTime + ", id=" + id + ", role=" + role + "]";
     }
 
 }
