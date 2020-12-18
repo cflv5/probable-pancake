629391cca02caae8c76c2addb85fc16c32b20419
clfv5 <bedirhanakcay@icloud.com>
2020-12-19 01:03:42 +0300

    UserStatus now serialize and deserialize with name value

--- src/main/java/tr/edu/yildiz/yazilimkalite/librarymanagement/model/UserStatus.java
+++ src/main/java/tr/edu/yildiz/yazilimkalite/librarymanagement/model/UserStatus.java
@@ -1,13 +1,18 @@
 package tr.edu.yildiz.yazilimkalite.librarymanagement.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public enum UserStatus {
-    ACTIVE("Aktif"), PASSIVE("Pasif"), DELETED("Silinmiş");
+    @JsonProperty("Aktif")
+    ACTIVE("Aktif"),
+
+    @JsonProperty("Pasif")
+    PASSIVE("Pasif"),
+
+    @JsonProperty("Silinmiş")
+    DELETED("Silinmiş");
 
     private final String name;
-
-    UserStatus() {
-        name = "";
-    }
 
     UserStatus(String name) {
         this.name = name;
