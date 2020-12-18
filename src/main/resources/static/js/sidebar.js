534eac9fc4b29ee9e2890f0b1e0fb5786b97ed30
clfv5 <bedirhanakcay@icloud.com>
2020-12-18 16:56:07 +0300

    side menu items urls are reworked

--- src/main/resources/static/js/sidebar.js
+++ src/main/resources/static/js/sidebar.js
@@ -1,7 +1,7 @@
-$(document).ready(function (){
-    $("#menu-toggle").click(function(e) {
+$(document).ready(function () {
+    $("#menu-toggle").click(function (e) {
         e.preventDefault();
-        const innerHTML = $("#menu-toggle").html() === "&lt;" ? "&gt;" : "&lt;";
+        const innerHTML = $("#wrapper").hasClass("toggled") ? `<em class="fas fa-arrow-left"></em>` : `<em class="fas fa-arrow-right">`;
         $("#wrapper").toggleClass("toggled");
         $("#menu-toggle").html(innerHTML);
     });
