37d9cbf685444177f9f30e0f3eeab7e2c677b872
clfv5 <bedirhanakcay@icloud.com>
2020-12-18 16:53:32 +0300

    LibrarySettingType is now stored with its name

--- src/main/java/tr/edu/yildiz/yazilimkalite/librarymanagement/model/LibrarySetting.java
+++ src/main/java/tr/edu/yildiz/yazilimkalite/librarymanagement/model/LibrarySetting.java
@@ -2,6 +2,8 @@ package tr.edu.yildiz.yazilimkalite.librarymanagement.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -17,6 +19,8 @@ public class LibrarySetting {
 
     private String value;
 
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private LibrarySettingType type;
 
     public LibrarySetting() {
