85d15d0fc097ca0e9b5283e51696b343f925dcdd
clfv5 <bedirhanakcay@icloud.com>
2020-12-20 16:04:48 +0300

    Security checks added to ensure authorized user can see

--- src/main/resources/static/js/sidebar.js
+++ src/main/resources/static/js/sidebar.js
@@ -1,8 +1,6 @@
 $(document).ready(function () {
     $("#menu-toggle").click(function (e) {
         e.preventDefault();
-        const innerHTML = $("#wrapper").hasClass("toggled") ? `<em class="fas fa-arrow-left"></em>` : `<em class="fas fa-arrow-right">`;
         $("#wrapper").toggleClass("toggled");
-        $("#menu-toggle").html(innerHTML);
     });
 });
